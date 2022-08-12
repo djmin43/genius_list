7a9a55a613c275fe609a75ccdf35f455219528cd
jay <dj.min43@gmail.com>
2022-08-12 20:02:15 +0900

    use fragment

--- src/queries/programmers.ts
+++ src/queries/programmers.ts
@@ -1,15 +1,4 @@
-import { IGatsbyImageData } from "gatsby-plugin-image";
 import { graphql } from "gatsby";
-
-export interface IProgrammer {
-  id: string;
-  url?: string;
-  name: string;
-  description?: string;
-  mainLanguage?: string;
-  link?: string;
-  image: IGatsbyImageData | null;
-}
 
 export const programmerFragment = graphql`
   fragment ProgrammerFragment on ContentfulPage {
